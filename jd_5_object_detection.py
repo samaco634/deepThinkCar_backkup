0b5d6464b2b8d7325475f38bffd3f9be5c3a3118
samaco634 <72288990+samaco634@users.noreply.github.com>
2023-01-08 00:18:31 +0900

    stop sign detect

--- jd_5_object_detection.py
+++ jd_5_object_detection.py
@@ -1,47 +1,47 @@
+'''
+1. importing necessary modules
+'''
 import cv2
-import socket
 from adafruit_servokit import ServoKit
 from jd_deep_lane_detect import JdDeepLaneDetect
 from jd_car_motor_l9110 import JdCarMotorL9110
+from jd_object_detect_thread import JdObejctDetectThread
 import time
-import imutils
-import base64
 
 '''
-Raspberry pi IP address. Put your Raspberry pi IP address here.
-Port number for remote control data
-Port number for video streaming  
+2. Creating object from classes
+  1) Servo handling object from ServoKit class 
+  2) Deep learning lane detecting object from JdDeepLaneDetect class 
+  3) DC motor handling object form JdCarMotorL9110 class 
 '''
-PI_IP = '172.31.98.78'
-TCP_PORT = 9998
-VIDEO_PORT = 9999
-OFFSET = 0
-
-BUFF_SIZE = 65536
-
-deep_detector = JdDeepLaneDetect("./models/lane_navigation_final.h5")
+# Deep learning detector object
+deep_detector = JdDeepLaneDetect("./models/lane_navigation_check.h5")
+# DC motor object
 motor = JdCarMotorL9110()
+# Servo object 
 servo = ServoKit(channels=16)
 
+'''
+3. Creating camera object and setting resolution of camera image
+cv2.VideoCapture() function create camera object.  
+'''
+# Camera object: reading image from camera 
 cap = cv2.VideoCapture(0)
+# Setting camera resolution as 320x240
 cap.set(3, 320)
 cap.set(4, 240)
 
-servo_offset = 5
+'''
+4. Preparing deepThinkCar starting.
+Before start driving, we need to adjust servo offset(wheel calibraton) 
+and wheel control while motor stop.
+It will prevents mis-driving of deepThinkCar. 
+'''
+# Servo offset. You can get offset from calibration.py
+servo_offset = 0
 servo.servo[0].angle = 90 + servo_offset
 
-# Setting UDP socket for video streaming (Rpi -> PC)
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-host_name = socket.gethostname()
-socket_address = (PI_IP, VIDEO_PORT)
-server_socket.bind(socket_address)
-print('Listening at:',socket_address)
-
-print('Waiting UDP connection... ')
-msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
-print('GOT connection from ',client_addr)
-
+# Prepare real starting 
 for i in range(30):
     ret, img_org = cap.read()
     if ret:
@@ -50,42 +50,57 @@ for i in range(30):
             print("can't find lane...")
         else:
             print(angle_deep)
-            servo.servo[0].angle = angle_deep + servo_offset			
+            if angle_deep > 40 and angle_deep < 140:
+                servo.servo[0].angle = angle_deep + servo_offset	
+            		
             cv2.imshow("img_angle", img_angle)
             cv2.waitKey(1)
     else:
         print("cap error")
 
+'''
+5. Starting motor before real driving 
+'''
+# Start motor 
 motor.motor_move_forward(30)
 
+# Start Obejct detect
+objectDetectThread = JdObejctDetectThread(cap)
 
+'''
+6. Perform real driving
+In this part, we perform real OpenCV lane detecting driving.
+When you press 'q' key, it stp deepThinkCar.
+While on driving, driving is recorded. 
+'''
+# real driving routine
 while cap.isOpened():
-    ret, img_org = cap.read()
-   
-    frame = imutils.resize(img_org,width=400)
-    # Encoding as JPEG 
-    encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
-    message = base64.b64encode(buffer)
-    try:
-        # Sending to client 
-        server_socket.sendto(message,client_addr)
-    except:
-        print("UDP communication error")
-    angle_deep, img_angle = deep_detector.follow_lane(img_org)
-    if img_angle is None:
-        print("can't find lane...")
-    else:
-        print(angle_deep)
-        servo.servo[0].angle = angle_deep + servo_offset
-        cv2.imshow("img_angle", img_angle)
+    
+    isStop, isImg, stopImage = objectDetectThread.getStopSign()
+    
+    if  isStop == False:
+        ret,  img_org = cap.read()
+        # Find lane angle
+        if ret :
+            angle_deep, img_angle = deep_detector.follow_lane(img_org)
+            if img_angle is None:
+                print("can't find lane...")
+            else:
+                print(angle_deep)
+                if angle_deep > 40 and angle_deep < 140:
+                    servo.servo[0].angle = angle_deep + servo_offset
+                cv2.imshow("img_angle", img_angle)
+    if isImg:
+        cv2.imshow('object detection', stopImage)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-server_socket.close()
+'''
+7. Finishing the driving
+Releasing occupied resources
+'''
+objectDetectThread.terminate() 
+motor.motor_stop()
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-        
-
+objectDetectThread.join()
---
+++ jd_object_detect_thread.py
@@ -0,0 +1,124 @@
+import threading
+
+import sys
+
+import cv2
+from tflite_support.task import core
+from tflite_support.task import processor
+from tflite_support.task import vision
+
+_MARGIN = 10  # pixels
+_ROW_SIZE = 10  # pixels
+_FONT_SIZE = 1
+_FONT_THICKNESS = 1
+_TEXT_COLOR = (0, 0, 255)  # red
+
+stop_width, stop_height = 60, 60
+
+# Define the thread that will continuously pull frames from the camera
+class JdObejctDetectThread(threading.Thread):
+    def __init__(self, camera, name='jd-object-detect-thread', model = './models/efficientdet_lite0.tflite', frameWidth = 320, frameHeight = 240, numThreads = 4, enableEdgeTPU = False ):
+        self.camera = camera
+        self.last_frame = None
+        super(JdObejctDetectThread, self).__init__(name=name)
+        self.running = True
+        self.start()
+        self.ret = False
+        self.stopSign = False
+        self.model = model
+        self.frameWidth = frameWidth
+        self.frameHeight = frameHeight
+        self.numThreads = numThreads
+        self.enableEdgeTPU = enableEdgeTPU
+        
+    def run(self):
+        while self.running:
+            self.ret, self.last_frame = self.camera.read()
+                        
+            # Visualization parameters
+            row_size = 20  # pixels
+            left_margin = 24  # pixels 
+            text_color = (0, 0, 255)  # red
+            font_size = 1
+            font_thickness = 1
+            fps_avg_frame_count = 10
+            
+            # Initialize the object detection model          
+            base_options = core.BaseOptions(
+                  file_name=self.model, use_coral=self.enableEdgeTPU, num_threads=self.numThreads)
+            detection_options = processor.DetectionOptions(category_name_allowlist=['stop sign'],
+              max_results=3, score_threshold=0.3)
+            options = vision.ObjectDetectorOptions(
+              base_options=base_options, detection_options=detection_options)
+            detector = vision.ObjectDetector.create_from_options(options)
+            
+            # Continuously capture images from the camera and run inference
+            if not self.ret:
+              sys.exit(
+                  'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+              )
+
+            # Convert the image from BGR to RGB as required by the TFLite model.
+            rgb_image = cv2.cvtColor(self.last_frame, cv2.COLOR_BGR2RGB)
+            
+            # Create a TensorImage object from the RGB image.
+            input_tensor = vision.TensorImage.create_from_array(rgb_image)
+            
+            # Run object detection estimation using the model.
+            detection_result = detector.detect(input_tensor)
+            
+            if len(detection_result.detections) > 0 : 
+                # Draw keypoints and edges on input image
+                for detection in detection_result.detections:
+                    # Draw bounding_box
+                    bbox = detection.bounding_box
+                    start_point = bbox.origin_x, bbox.origin_y
+                    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+                    #print(f"width : {bbox.width}, height : {bbox.height}")
+                    cv2.rectangle(self.last_frame, start_point, end_point, _TEXT_COLOR, 3)
+                    
+                    # Draw label and score
+                    category = detection.categories[0]
+                    category_name = category.category_name
+                    probability = round(category.score, 2)
+                        
+                    result_text = category_name + ' (' + str(probability) + ')'
+                    
+                    if ( category_name == 'stop sign'):
+                        if(bbox.width > stop_width or bbox.height > stop_height):
+                            print( bbox.width , ", ",  bbox.height)
+                            self.stopSign = True
+                        
+                    text_location = (_MARGIN + bbox.origin_x,
+                                     _MARGIN + _ROW_SIZE + bbox.origin_y)
+                    cv2.putText(self.last_frame, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+            else:
+                    self.stopSign = False
+            
+
+        
+    def getStopSign(self):
+        print("stop Sign is Called : " , self.stopSign)
+        return self.stopSign, self.ret, self.last_frame
+    
+    def terminate(self):
+        print('terminating thread')
+        self.running = False
+
+if __name__ == '__main__':
+    camera = cv2.VideoCapture(-1)
+    camera.set(3, 320)
+    camera.set(4, 240)
+    camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    
+    objectDetectThread = JdObejctDetectThread(camera)
+    
+    while 1:
+        isStop, isImg, stopImage = objectDetectThread.getStopSign()
+        if isImg:
+            cv2.imshow('object detection', stopImage)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            objectDetectThread.join()
+            break
+    
